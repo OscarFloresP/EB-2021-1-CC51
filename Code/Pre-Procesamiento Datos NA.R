04c0c9d45fb94cc3bc37acdac4f2b217d1d57a35
carlos375 <51143778+carlos375@users.noreply.github.com>
2021-07-02 18:59:39 -0500

    Eliminando filas con video_id vacio

--- Code/Pre-Procesamiento Datos NA.R
+++ Code/Pre-Procesamiento Datos NA.R
@@ -27,6 +27,14 @@ DFMexico_Limpio <- random.df(DFVideos_MX, c(8,9,10,11))
 Comprobacion_Aleatorio <- is.na(DFMexico_Limpio)
 summary(Comprobacion_Aleatorio)
 
+#Eliminando filas vacías en video_id
+View(DFMexico_Limpio)
+Vacios <- DFMexico_Limpio$video_id == ""
+summary(Vacios)
+DFMexico_Limpio <- subset(DFMexico_Limpio, video_id != "")
+Vacios <- DFMexico_Limpio$video_id == ""
+summary(Vacios)
+
 #Guardando Dataframe
 save(DFMexico_Limpio, file = "~R/EB-Admin-Info/Data/DF_Limpio.RData")
 write.csv(DFMexico_Limpio, "DFMexicoVideos_Limpio.csv", row.names = FALSE)
